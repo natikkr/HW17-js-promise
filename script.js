5640292a5e003d2847284aa1e586a8cfd67c183a
natikkr <82267830+natikkr@users.noreply.github.com>
2021-08-28 23:30:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-function delay(){
-    return new Promise(function(resolve, reject){
-
-    setTimeout(() => resolve("result"), 2000);
-    }) 
-   
-.then (
-        (result) => {
-            console.log("Hello! ");
-        },
-        (error) => {
-            console.log("Error "); 
-        }
-    
-    )
-
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
-delay()
+  delay(1000).then(() => console.log("Hello!"))
+  
+
 
 function getUserInfo(){
     return new Promise(function(resolve, reject){
@@ -26,26 +14,30 @@ function getUserInfo(){
         return new Promise(function(resolve,reject){
             userInfo.avatar = 'https://previews.123rf.com/images/stockgiu/stockgiu1708/stockgiu170802061/83728179-avatar-sketch-of-a-funny-man-s-haed-with-sunglasses-and-hairstyle-design.jpg'
             setTimeout(() => resolve(userInfo), 1000);
-          });
-        })
+        });
+    })
 .then(
     function getUserAdditionalInfo(userInfo) {
         return new Promise(function(resolve, reject) {
-        userInfo.interests = ['sport', 'books'];
-        setTimeout(() => resolve(userInfo), 1000);
+            userInfo.interests = ['sport', 'books'];
+            setTimeout(() => resolve(userInfo), 1000);
         });
     })      
 }
-console.log(getUserInfo())  
+console.log(getUserInfo().then().then().then())  
+
+
 
 new Promise(function(resolve, reject) {
     setTimeout(() => resolve({ name: 'Vic', age: 21, id: 1 } ), 1000);
-  })
-.then(function(userInfo) {
-    return new Promise(function(resolve, reject) {
-    setTimeout(() => reject(new Error('wrong data') ), 1000);
     })
-.catch(function(error) {
-    console.log(error)
-})
+.then( 
+    function(userInfo) {
+        return new Promise(function(resolve, reject) {
+            setTimeout(() => reject(new Error('wrong data') ), 1000);
+    })
+.catch(
+    function(error) {
+        console.log(error)
+    })
 })
